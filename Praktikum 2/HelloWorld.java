da9c914718e3f59d6a7982f9f7d8953db682912d
Suryana-7 <Suryana-7@users.noreply.github.com>
2020-09-17 14:39:40 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("NAMA  :   SURYANA");
+		System.out.println("NIM   :   6706202120");
+		System.out.println("KELAS :   D3IF4403");
+		}
+}
